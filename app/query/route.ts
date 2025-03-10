0fddb1e698d45a756c3f28a5012f9c42df593453
jose <idk@asdf.com>
2025-03-10 10:29:15 +0100

    test

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,14 +4,14 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 console.log(process.env.POSTGRES_URL)
 
 async function listInvoices() {
-	const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+	// const data = await sql`
+  //   SELECT invoices.amount, customers.name
+  //   FROM invoices
+  //   JOIN customers ON invoices.customer_id = customers.id
+  //   WHERE invoices.amount = 666;
+  // `;
 
-	return data;
+	return process.env.POSTGRES_URL;
 }
 
 export async function GET() {
